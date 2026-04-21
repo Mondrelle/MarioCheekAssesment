711923392f826f004b170fcde23985ae596db1a1
Mondrelle <mario_cheek10@mymail.eku.edu>
2026-04-21 11:15:10 -0400

    Initial Commit

---
+++ MarioCheekAssesment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarioCheekAssesment
+//
+//  Created by Mondrelle Cheek on 4/21/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
