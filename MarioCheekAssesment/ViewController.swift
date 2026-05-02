b4fe414a2ba307a5071afa721f217d47bb3386d0
Mondrelle <mario_cheek10@mymail.eku.edu>
2026-05-02 10:52:32 -0400

    Auto Layout finished

--- MarioCheekAssesment/ViewController.swift
+++ MarioCheekAssesment/ViewController.swift
@@ -3,12 +3,21 @@
 //  MarioCheekAssesment
 //
 //  Created by Mondrelle Cheek on 4/21/26.
-//
+//  BMI Calculator
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var userHeight: UITextField!
+    @IBOutlet weak var userWeight: UITextField!
+    @IBOutlet weak var feedbackLabel: UILabel!
+    @IBOutlet weak var calculateBMI: UIButton!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
